26edf0b377439e06aec5795387ddaedb53f5c613
CHACCHAN <chacchan.info@gmail.com>
2023-12-31 13:49:29 +0900

    初回コミット

---
+++ resources/views/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    {{-- フォント --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;400&family=Poppins:wght@300&display=swap" rel="stylesheet">
+    {{-- 全体設定 --}}
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <div id="app">
+        {{-- Vue --}}
+        <router-view></router-view>
+    </div>
+    @vite('resources/js/app.js')
+</body>
+</html>
