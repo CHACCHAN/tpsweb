a5c6b2601afd1506496e2b24de3358752e88ac18
CHACCHAN <chacchan.info@gmail.com>
2023-12-31 15:14:15 +0900

    ルーティンの変更

--- resources/js/router.js
+++ resources/js/router.js
@@ -2,12 +2,12 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
     {
-        path: "/tpsweb/public/about",
+        path: "/tps/about",
         component: () => import('./views/AboutView.vue'),
         name: "about"
     },
     {
-        path: "/tpsweb/public/",
+        path: "/tps/",
         component: () => import('./views/HomeView.vue'),
         name: "home"
     }
